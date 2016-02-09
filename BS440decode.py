1749292fb152c7098e901a75b629a38b129ff6fc
Keptenkurk <paulmerkx@gmail.com>
2016-02-09 20:59:22 +0100

    added ini and mailing

--- BS440decode.py
+++ BS440decode.py
@@ -1,20 +1,21 @@
 from struct import *
 '''
-Reads Medisana Scale hex Indication and decodes scale 
+Reads Medisana BS440 Scale hex Indication and decodes scale
 values from hex data string
-This class provides 3 functions: 
+This class provides 3 functions:
 decodePerson, decodeWeight, decodeBody
-Each function receives the hex handle and bytevalues and 
+Each function receives the hex handle and bytevalues and
 returns a dictionary with decoded data
 '''
-    
+
+
 def decodePerson(handle, values):
     '''
     decodePerson
     handle: 0x25
     values[0] = 0x84
-    Returns a dict for convenience: 
-        valid (True, False) 
+    Returns a dict for convenience:
+        valid (True, False)
         person (1..9)
         gender (male|female)
         age (0..255 years)
@@ -24,47 +25,51 @@ def decodePerson(handle, values):
     data = unpack('BxBxBBBxB', bytes(values[0:9]))
     retDict = {}
     retDict["valid"] = (handle == 0x25 and data[0] == 0x84)
+    retDict["type"] = 'person'
     retDict["person"] = data[1]
     if data[2] == 1:
         retDict["gender"] = "male"
     else:
         retDict["gender"] = "female"
     retDict["age"] = data[3]
-    retDict["size"] = data[4]        
+    retDict["size"] = data[4]
     if data[5] == 3:
         retDict["activity"] = "high"
     else:
         retDict["activity"] = "normal"
     return retDict
 
+
 def decodeWeight(handle, values):
     '''
-    decodeWeight    
+    decodeWeight
     Handle: 0x1b
     Byte[0] = 0x1d
     Returns:
         valid (True, False)
         weight (5,0 .. 180,0 kg)
-        timestamp (unix timestamp date and time of measurement) 
+        timestamp (unix timestamp date and time of measurement)
         person (1..9)
-        note: in python 2.7 to force results to be floats devide by float.
+        note: in python 2.7 to force results to be floats,
+        devide by float.
         '''
     data = unpack('<BHxxIxxxxB', bytes(values[0:14]))
     retDict = {}
     retDict["valid"] = (handle == 0x1b and data[0] == 0x1d)
     retDict["weight"] = data[1]/100.0
-    retDict["timestamp"] = data[2] 
+    retDict["timestamp"] = data[2]
     retDict["person"] = data[3]
     return retDict
-    
+
+
 def decodeBody(handle, values):
     '''
-    decodeBody    
+    decodeBody
     Handle: 0x1e
     Byte[0] = 0x6f
     Returns:
         valid (True, False)
-        timestamp (unix timestamp date and time of measurement) 
+        timestamp (unix timestamp date and time of measurement)
         person (1..9)
         kcal = (0..65025 Kcal)
         fat = (0..100,0 %)  percentage of body fat
@@ -72,7 +77,7 @@ def decodeBody(handle, values):
         muscle = (0..100,0 %) percentage of muscle
         bone = (0..100,0) bone weight
         note: in python 2.7 to force results to be floats: devide by float.
-    '''     
+    '''
     data = unpack('<BIBHHHHH', bytes(values[0:16]))
     retDict = {}
     retDict["valid"] = (handle == 0x1e and data[0] == 0x6f)
